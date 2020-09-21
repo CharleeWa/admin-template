c613aace32da6c49660b227811ca8b1506dd2d0a
Charlie <18888351756@163.com>
2020-09-21 14:53:46 +0800

    feat：配置文件、依赖和插件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ProLayout, { PageHeaderWrapper } from '@ant-design-vue/pro-layout'
+import themePluginConfig from '../config/themePluginConfig'
+
+import bootstrap from './core/bootstrap'
+import './core/lazy_use'
+import './global.less'
+
+Vue.config.productionTip = false
+
+Vue.component('pro-layout', ProLayout)
+Vue.component('page-header-wrapper', PageHeaderWrapper)
+
+window.umi_plugin_ant_themeVar = themePluginConfig.theme
 
 new Vue({
   router,
   store,
+  created: bootstrap,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
